e3bf60fe6cc4e96a11b4afefcee961f485dd1671
Max <33336634+Venturh@users.noreply.github.com>
2023-02-12 18:34:43 +0100

    adjust nodemailer transport options

--- src/pages/api/contact-form.ts
+++ src/pages/api/contact-form.ts
@@ -3,9 +3,9 @@ import { SendMailOptions, createTransport } from 'nodemailer'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	const transporter = createTransport({
-		host: 'smtp.ethereal.email', //mail.privateemail.com
-		port: 587, //465
-		// 	secure: true,
+		host: 'mail.privateemail.com',
+		port: 465,
+		secure: true,
 		auth: {
 			user: process.env.PRIVATE_MAIL,
 			pass: process.env.PRIVATE_MAIL_PASSWORD,
@@ -13,16 +13,18 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	})
 	const body = req.body
 	const msg: SendMailOptions = {
-		from: body.email,
+		from: process.env.PRIVATE_MAIL,
 		to: process.env.PRIVATE_MAIL,
-		subject: 'Neue Nachricht von ' + body.name,
-		html: `<div> <h1>Neue Nachricht von ${body.name}</h1> <div>Exhibit: ${body.exhibit}</div> <p>Email: ${body.email}</p> <p>Nachricht: ${body.message}</p> </div>`,
+		subject: `Werkanfrage zu ${body.exhibit}`,
+		html: `<div> <h1>Neue Nachricht von ${body.name}</h1>
+        <p>Email: ${body.email}</p> <p>Nachricht: ${body.message}</p> </div>`,
 	}
 
 	try {
 		await transporter.sendMail(msg)
 		return res.status(200).end()
 	} catch (error) {
+		console.log(error)
 		return res.status(500).end()
 	}
 }
