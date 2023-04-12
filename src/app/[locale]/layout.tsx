43353e9564f881446be7d5c9f1630c83d29639ed
Max <33336634+Venturh@users.noreply.github.com>
2023-04-12 16:19:30 +0200

    adjust umami url

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -50,7 +50,7 @@ export default async function RootLayout({ children, params }) {
 				async
 				defer
 				data-website-id="c73161e2-5632-42ac-a5e6-2b0b8e746b89"
-				src="umami-analytics-sage.vercel.app/umami.js"
+				src="/stats/script.js"
 			/>
 			<head></head>
 			<body className="h-full">
