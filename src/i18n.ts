ea9f10338870a48c1cf6b5c8fbe581f05c936ef8
Max <33336634+Venturh@users.noreply.github.com>
2023-04-19 15:19:27 +0200

    maybe

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,5 @@
 import { getRequestConfig } from 'next-intl/server'
 
-import { languages } from './sanity/i18n'
-
 export default getRequestConfig(async ({ locale }) => ({
 	messages: (await import(`../messages/${locale}.json`)).default,
 }))
