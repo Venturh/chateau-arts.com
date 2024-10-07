a91323921aa8f4af28344057ca5711a96ddb22a5
Max <33336634+Venturh@users.noreply.github.com>
2024-10-07 20:01:19 +0200

    change favicon

--- src/app/studio/head.tsx
+++ src/app/studio/head.tsx
@@ -3,7 +3,11 @@ export default function Head() {
 		<>
 			<title></title>
 			<meta content="width=device-width, initial-scale=1" name="viewport" />
-			<link rel="icon" href="/favicon.ico" />
+			<link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+			<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+			<link rel="shortcut icon" href="/favicon.ico" />
+			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+			<link rel="manifest" href="/site.webmanifest" />
 		</>
 	)
 }
